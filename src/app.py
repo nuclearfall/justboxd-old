11947d17a3ad0df26f7594bb7b4d98bf70d0e6e4
Nagasaki Nightmare <nuclearfall@protonmail.com>
2023-04-06 07:58:45 -0500

    Various changes in dev. Many unlikely to ever see the light of day after new ui file and classes are committed.

--- src/app.py
+++ src/app.py
@@ -28,6 +28,7 @@ from PySide6.QtWidgets import (
     QButtonGroup
 )
 
+
 class WorkerSignals(QObject):
     '''
     Defines the signals available from a running worker thread.
@@ -106,12 +107,16 @@ class MainWindow(QMainWindow):
 
         service_names = [val for val in justboxd.subscription_services.values()]
         country_code_list = [c.alpha_2 for c in pycountry.countries]
-        self.my_services = justboxd.free_services
-        
+        self.my_services = justboxd.get_user_services()
         self.threadpool = QThreadPool()
 
         self.setWindowTitle("JustBoxd")
         self.layout = QVBoxLayout()
+
+        urlLabel = QLabel("Letterboxd Url")
+        self.urlWidget = QLineEdit()
+        self.layout.addWidget(urlLabel)
+        self.layout.addWidget(self.urlWidget)
 
         usernameLabel = QLabel("Letterboxd Username")
         self.usernameWidget = QLineEdit()
@@ -136,9 +141,9 @@ class MainWindow(QMainWindow):
         completer = QCompleter(service_names)
         completer.setCaseSensitivity(Qt.CaseInsensitive)
         completer.setCompletionMode(QCompleter.InlineCompletion)
-        completer.activated.connect(self.add_service)
+        completer.activated.connect(self.addService)
         self.lineEdit.setCompleter(completer)
-        self.lineEdit.returnPressed.connect(self.add_service)
+        self.lineEdit.returnPressed.connect(self.addService)
         self.layout.addWidget(servicesLabel)
         self.layout.addWidget(self.lineEdit)
 
@@ -151,6 +156,11 @@ class MainWindow(QMainWindow):
         self.layout.addWidget(removeServiceLabel)
         self.layout.addWidget(self.box)
 
+
+        self.saveServicesButton = QPushButton("Save Services", self)
+        self.saveServicesButton.pressed.connect(self.saveUserServices)
+        self.layout.addWidget(self.saveServicesButton)
+
         self.searchButton = QPushButton("Search", self)
         self.searchButton.pressed.connect(self.submitSearch)
         self.layout.addWidget(self.searchButton)
@@ -159,34 +169,35 @@ class MainWindow(QMainWindow):
         self.widget.setLayout(self.layout)
         self.setCentralWidget(self.widget)
 
+    def saveUserServices(self):
+        justboxd.save_user_services(self.my_services)
+
     def submitSearch(self):
-
-        alpha2 = self.country_codes.currentText()
-        username = self.usernameWidget.text()
-        lbxd_list = self.listWidget.text()
-
-        worker = Worker(self.serviceSearch, username, alpha2, lbxd_list) # Any other args, kwargs are passed to the run function
+        self.searchButton.setEnabled(False)
+        worker = Worker(self.serviceSearch) # Any other args, kwargs are passed to the run function
         worker.signals.result.connect(self.print_output)
         worker.signals.finished.connect(self.thread_complete)
         worker.signals.progress.connect(self.progress_fn)
-        self.searchButton.setEnabled(False)
+
         self.threadpool.start(worker)
 
-
-    def serviceSearch(self, username, alpha2, lbxd_list, progress_callback):
-        titles = justboxd.get_watchlist_titles(username, lbxd_list)
+    def serviceSearch(self, progress_callback):
+        alpha2 = self.country_codes.currentText()
+        username = self.usernameWidget.text()
+        list_name = self.listWidget.text()
+        url_string = self.urlWidget.text()
+        titles = justboxd.get_watchlist_titles(username=username, 
+                                                url_string=url_string, list_name=list_name)
         services_short = [k for k in self.my_services.keys()]
+        free_services_short = [k for k in justboxd.free_services.keys()]
+        services_short = services_short + free_services_short
         streaming_from = justboxd.get_streamers(titles,
                                 services_short, country=alpha2)
-        local_html = justboxd.save_json_to_html(streaming_from, 'streaming.html')
-        local_html = 'file:///' + os.getcwd() + "/" + local_html
-        QDesktopServices.openUrl(QUrl(local_html))
-        
-        progress_callback.emit(n*100/4)
+        local_html = justboxd.save_json_to_html(streaming_from)
+        justboxd.save_to_csv(streaming_from) 
+        QDesktopServices.openUrl(QUrl.fromLocalFile(local_html))
+        self.searchButton.setEnabled(True)
         return "Done."
-
-    def progress_fn(self, n):
-        print("%d%% done" % n)
 
     def print_output(self, s):
         print(s)
@@ -194,7 +205,20 @@ class MainWindow(QMainWindow):
     def thread_complete(self):
         print("THREAD COMPLETE!")
 
-    def add_service(self):
+    def progress_fn(self, n):
+        print("%d%% done" % n)
+
+    def initUserServices(self):
+        if self.my_services is None:
+            self.my_services = {}
+        else:
+            for v in self.my_services.values():
+                button = QPushButton(v)
+                self.box_layout.addWidget(button)
+                self.group.addButton(button)
+            self.layout.update()
+
+    def addService(self):
         for k, v in justboxd.service_codes.items():
             if self.lineEdit.text() == v and v not in self.my_services.values():
                 self.my_services[k] = v
@@ -219,5 +243,6 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
+window.initUserServices()
 
 app.exec_()
--- src/justboxd.py
+++ src/justboxd.py
@@ -1,3 +1,4 @@
+import os
 import urllib.request
 import ssl
 import json
@@ -5,10 +6,11 @@ from json2html import *
 from bs4 import BeautifulSoup
 from justwatch import JustWatch
 from service_codes import service_codes
+import csv
 
 # create a dictionary of all service codes 
-with open('../providers.json') as fn:
-	providers = json.load(fn)
+with open('../providers.json') as fp:
+	providers = json.load(fp)
 
 service_names = {p['short_name']:p['clear_name'] for p in providers}
 service_codes_short = [k for k in service_names.keys()]
@@ -18,19 +20,48 @@ free_services = {p['short_name']:p['clear_name'] for p in providers \
 					'flatrate' not in p['monetization_types']}
 subscription_services = {p['short_name']:p['clear_name'] for p in providers if 'flatrate' in p['monetization_types']}
 
+def get_user_services(save_file="user_services.json"):
+	if os.path.exists(save_file):
+		with open(save_file) as fp:
+			json_data = json.load(fp)
+			return json_data
+	else:
+		return None
 
-def get_watchlist_titles (letterboxd_username='default', 
-			list_name='watchlist', sort_by='popular', pages=10):
+def save_user_services(services, save_file="user_services.json"):
+	with open(save_file, 'w') as fp:
+		json_data = json.dumps(services, indent=4)
+		fp.write(json_data)
+		fp.close()
+		return save_file
+
+# def parse_lxbd_url(lbxd_url):
+# 	lbxd_url = "https://letterboxd.com/"
+# 	url_list = url_string.split("/")
+# 	url_list = list(filter(lambda x: x not in ["https:", "", "letterboxd.com"], url_list))
+# 	if len(url_list) == 1:
+# 		return lbxd_url
+
+def get_watchlist_titles (username='default', 
+			list_name='watchlist', url_string='', sort_by='', pages=10):
 	titles = []
-	url_string = 'https://letterboxd.com/' + letterboxd_username + '/'
-	url_postfix = "/by/" + sort_by + "/page/"
-	if list_name != "watchlist":
-		list_name = ''.join(["-" if char == " " else "" if char == "'" else char for char in list_name]).lower()
-		list_name = "list/" + list_name
-	url_string += list_name + url_postfix
+	if url_string == '':
+		url_string = 'https://letterboxd.com/' + username + '/'
+		if list_name != "watchlist":
+			list_name = ''.join(["-" if char == " " else "" if char == "'" else char for char in list_name]).lower()
+			url_string += "list/" + list_name
+		else:
+			url_string += "watchlist/"
+	page_postfix = "page/" if url_string[-1] == "/" else "/page/"
+	if sort_by != '':
+		url_postfix = "/by/" + sort_by + page_postfix
+		url_string.split("/")
+	else:
+		url_postfix = page_postfix
+	url_string += url_postfix
 	ssl._create_default_https_context = ssl._create_unverified_context
 	html_str = ''
-
+	print(url_string)
 	for i in range(pages):
 		fp = urllib.request.urlopen(url_string + str(i+1))
 		html_str += fp.read().decode("utf8")
@@ -40,19 +71,24 @@ def get_watchlist_titles (letterboxd_username='default',
 	html_objects = soup.findAll('li', {'class': 'poster-container'})
 	for html_object in html_objects:
 		object_str = str(html_object).replace('&amp;','&')
-		print(object_str)
 		start_index = object_str.index('<img alt="')+10
 		end_index = object_str.index('"', start_index)
 		movie_name = object_str[start_index:end_index]
-		print(movie_name)
 		titles.append(movie_name)
+
 	return titles
+
+
+
 
 # get list of streaming services for list of titles.
 def get_streamers(watchlist_titles, streamers, country='US'):
 	just_watch = JustWatch(country=country)
 	results = {}
+	# nonefound = []
+
 	for movie in watchlist_titles:
+		print(movie)
 		offers = []
 		movie_found = just_watch.search_for_item(
 				query=movie, 
@@ -64,17 +100,33 @@ def get_streamers(watchlist_titles, streamers, country='US'):
 			offer = [s for s in offer if s in streamers]
 			offer = [*set(offer)]
 			offer_lookup = [service_codes[k] for o in offer for k in service_codes.keys() if o == k]
-			if offer_lookup != []:
-				full_key = movie + " " + "(" + str(movie_year) + ")"
-				results[full_key] = offer_lookup
+			title_and_year = movie + " " + "(" + str(movie_year) + ")"
+			results[title_and_year] = offer_lookup
+		else:
+			nonefound.append(title_and_year)
+		# print(json.dumps({"Title":full_key, "Streaming Services": [v for k, v in streaming_services.items() if k in offer]}))
 	return results
 
 def save_json_to_html(titles_dict, fn='streaming.html'):
 	json_data = json.dumps(titles_dict, indent=4)
-	jhtml = json2html.convert(json = json_data)
 	with open(fn, 'w') as fp:
-		fp.write(jhtml)
+		fp.write(json2html.convert(json=json_data))
 	return fn
 
+def save_to_csv(titles_dict, fn='services.csv'):
+	with open('names.csv', 'w', newline='') as csvfile:
+		fieldnames = ['movie', 'services']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		for k, v in titles_dict.items():
+			writer.writerow({'movie':k, 'services': v})
+	return fn
 
+# def main():
+# 	username = 'grryboy'
+# 	titles = get_watchlist_titles(username)
+# 	streamers = free_services
+# 	save_to_csv(get_streamers(titles, streamers))
 
+# if __name__ == '__main__':
+# 	main()
